d0d9e04d2a45ab5f1a4f1bac23b216f69a0afb74
Dreamdreams8 <172611825@qq.com>
2024-01-31 09:43:06 +0800

    添加自定义数据生成kitti数据集脚本

--- tools/gen_kitti_data.py
+++ tools/gen_kitti_data.py
@@ -5,6 +5,10 @@ import os
 import numpy as np
 import json
 import shutil 
+import math
+from PIL import Image  
+
+class_map = {"Truck":"Truck","Car":"Car"}
 
 def mkdir(file_path):
     if os.path.exists(file_path):
@@ -83,9 +87,49 @@ def json2txt(json_paths,txt_paths):
                 rotation_x = json_data[i]["psr"]["rotation"]["x"]
                 rotation_y = json_data[i]["psr"]["rotation"]["y"]
                 rotation_z = json_data[i]["psr"]["rotation"]["z"]
-                line = str(position_x) + " " + str(position_y) + " " + str(position_z) + " " + str(scale_x) + " " + str(scale_y) + " " + str(scale_z) + " " + str(rotation_z)  + " " + obj_type +  "\n"
+                alpha = round(rotation_z -  math.atan(position_x / position_y),2)
+                line = class_map[obj_type] + " " + "0.00" + " " + "0" + " " +  str(alpha)  + " " + "0.0" + " " + "0.0" + " " + "0.0"+ " " + "0.0" + " "  \
+                                    + str(scale_z) + " " + str(scale_y) + " " + str(scale_x)   + " "  + str(position_x)   + " " + str(position_y) + " " + str(position_z) + " " + str(rotation_z) +  "\n"
                 with open(txt_file_new,"a") as f:
                     f.write(line)
+
+def calib2calib(json_paths,calib_path):
+    json_path_list = os.listdir(json_paths)
+    for json_path in json_path_list:
+        # print(json_path)
+        (filename,extension) = os.path.splitext(json_path)
+        json_path_new = json_paths + "/" +json_path
+        txt_file_new = os.path.join(calib_path, filename) + '.txt'
+        P0 = "P0:" + " " + "1.0" + " " +  "0" + " " + "0" + " " + "0" + " " + "0" + " " + "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        P1 = "P1:" + " " +  "1.0" + " " + "0" + " " + "0" + " " + "0" + " " + "0.0"  + " " +   "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        P2 = "P2:" + " " +    "1.0" + " " + "0" + " " + "0" + " " + "0" + " " + "0.0" + " " +   "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        P3 = "P3:" + " " +    "1.0" + " " + "0" + " " + "0" + " " + "0" + " " + "0.0" + " " +   "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        R0_rect = "R0_rect:" + " " +"1.0" + " " +  "0" + " " + "0" + " " + "0" + " " + "0" + " " + "1.0" + " " + "0.0" + " " + "0.0" + " " + "1.0"+  "\n"
+        Tr_velo_to_cam = "Tr_velo_to_cam:" + " " +"1.0" + " " +  "0" + " " + "0" + " " + "0" + " " + "0" + " " + "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        Tr_imu_to_velo ="Tr_imu_to_velo:" + " " + "1.0" + " " +  "0" + " " + "0" + " " + "0" + " " + "0" + " " + "1.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "0.0" + " " + "1.0" + " " + "1.0" +  "\n"
+        with open(txt_file_new,"a") as f:
+            f.write(P0)
+            f.write(P1)
+            f.write(P2)
+            f.write(P3)
+            f.write(R0_rect)
+            f.write(Tr_velo_to_cam)
+            f.write(Tr_imu_to_velo)
+
+def img2img(json_paths,image_2_path):
+    json_path_list = os.listdir(json_paths)
+    for json_path in json_path_list:
+        # print(json_path)
+        (filename,extension) = os.path.splitext(json_path)
+        json_path_new = json_paths + "/" +json_path
+        img_file_new = os.path.join(image_2_path, filename) + '.png'
+        
+        # 创建一个1920x1080的空图片，所有像素设置为白色  
+        width = 1920  
+        height = 1080  
+        image = Image.new('RGB', (width, height), color='white')  
+        # 保存为PNG格式  
+        image.save(img_file_new)
 
 def txt2json(txt_paths,json_paths):
     txt_path_list = os.listdir(txt_paths)
@@ -160,7 +204,7 @@ def gen_train_test_name(pcdfolder,ImageSets_path,test_ratio):
     for i in train.tolist():
         with open(train_path,"a") as f:
             f.write(str(i) + "\n")    
-    for j in train.tolist():
+    for j in test.tolist():
         with open(test_path,"a") as f:
             f.write(str(j) + "\n")                
 
@@ -172,20 +216,39 @@ def gen_custom_data(custom_path,pcdfolder,labelsfolder,test_ratio):
         pass
     else:
         os.makedirs(ImageSets_path)        
+    training_path = custom_path + "/" + "training"    
+    if os.path.exists(training_path):
+        pass
+    else:
+        os.makedirs(training_path)       
     gen_train_test_name(pcdfolder,ImageSets_path,test_ratio)
-    points_path = custom_path + "/" + "points"
+    points_path = training_path + "/" + "velodyne"
     if os.path.exists(points_path):
         pass
     else:
         os.makedirs(points_path)       
-    pcd2npy(pcdfolder,points_path)    
+    pcd2bin(pcdfolder,points_path)    
 
-    labels_path = custom_path + "/" + "labels"
+    labels_path = training_path + "/" + "label_2"
     if os.path.exists(labels_path):
         pass
     else:
         os.makedirs(labels_path)           
     json2txt(labelsfolder,labels_path)    
+
+    image_2_path =  training_path + "/" + "image_2"
+    if os.path.exists(image_2_path):
+        pass
+    else:
+        os.makedirs(image_2_path)     
+    img2img(labelsfolder,image_2_path)    
+
+    calib_path =  training_path + "/" + "calib"
+    if os.path.exists(calib_path):
+        pass
+    else:
+        os.makedirs(calib_path)     
+    calib2calib(labelsfolder,calib_path)        
 
 def merge_pcd_label(pcd_label_folder):
     file_list = os.listdir(pcd_label_folder)
@@ -212,12 +275,53 @@ def merge_pcd_label(pcd_label_folder):
     return merge_path_pcd,merge_path_label
 
 
+def check_pcd_label(pcdfolder ,labelsfolder):
+    file_list = os.listdir(pcdfolder)
+    pcd = []
+    for file in file_list: 
+        (filename,extension) = os.path.splitext(file)
+        pcd.append(filename)
+
+    file_list = os.listdir(labelsfolder)
+    label = []
+    for file in file_list: 
+        (filename,extension) = os.path.splitext(file)
+        label.append(filename)
+    pcd = set(pcd)    
+    label = set(label)
+    print("len(pcd):     ",len(pcd))
+    print("len(label):     ",len(label))
+    non_intersection_1 = pcd - label    
+    non_intersection_2 = label - pcd
+    for file in non_intersection_1:
+        file_path = pcdfolder + "/" + file + ".pcd"
+        if os.path.exists(file_path):  
+            try:  
+                os.remove(file_path)  
+                print("文件已成功删除:  ",file_path)  
+            except OSError as e:  
+                print(f"删除文件时出错: {e}")        
+        else:
+            continue        
+    for file in non_intersection_2:
+        file_path = labelsfolder + "/" + file + ".json"
+        if os.path.exists(file_path):  
+            try:  
+                os.remove(file_path)  
+                print("文件已成功删除: ",file_path)  
+            except OSError as e:  
+                print(f"删除文件时出错: {e}")             
+        else:
+            continue           
+    print("check_pcd_label sucess!")   
+
 if __name__ == '__main__':
     pcd_label_folder = "/home/why/mnt/github_demo/labelcloud/SUSTechPOINTS/SUSTechPOINTS/data"
     # pcd_label_folder = "/home/why/mnt/github_demo/labelcloud/SUSTechPOINTS/SUSTechPOINTS/data_test"
     pcdfolder ,labelsfolder = merge_pcd_label(pcd_label_folder)
+    check_pcd_label(pcdfolder ,labelsfolder)
     # custom_path = "/home/why/mnt/github_demo/labelcloud/SUSTechPOINTS/SUSTechPOINTS/data/custom"
-    custom_path = "/home/why/mnt/github_demo/labelcloud/SUSTechPOINTS/SUSTechPOINTS/data/custom"
+    custom_path = "/home/why/mnt/github_demo/labelcloud/SUSTechPOINTS/SUSTechPOINTS/data/kitti_custom"
     test_ratio =   0.1
     # test_ratio =   0
     gen_custom_data(custom_path,pcdfolder,labelsfolder,test_ratio)
